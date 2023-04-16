113e19362e28262ae8f16cc33a5ee6582e556caf
Diego FloresJ <dfj270801@gmail.com>
2023-04-16 13:09:06 -0600

    Adding folders and base files

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/productos', (req, res) => {
-    const productos =  require('../assets/data/products.json')
+router.get('/weapons', (req, res) => {
+    const productos =  require('../assets/data/weapons/magic/magicWeapons.json')
     res.send(productos)
 });
 
-router.get('/users', (req, res) => {
-    const users =  require('../assets/data/users.json')
-    res.send(users)
-});
+// router.get('/users', (req, res) => {
+//     const users =  require('../assets/data/users.json')
+//     res.send(users)
+// });
 
 module.exports = router;
